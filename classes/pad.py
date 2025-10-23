9652cddc0d45db39c6a9147fa5a742c053e860ca
Mouad Ouamane <mouad.ouamane@cpe.fr>
2025-10-23 10:45:43 +0200

    Sauvegarde de mes changements locaux avant pull

--- classes/pad.py
+++ classes/pad.py
@@ -14,28 +14,34 @@ import tkinter as tk
     - afficher 
     - 4 fonctions pour le déplacement avec 4 flags 
     - une fonction get_coords qui permet de recuperer x et y d'un seul coup 
-    - une fonction update
+    - une fonction update qui gère le déplacement du pad
     """
 
 
 class Pad:
-    def __init__(self, canvas, x, y, vitesse = 5, largeur=120, hauteur=20, couleur="blue"):
+
+    # fonction d'initialisation des attribur de l'objet pad
+    def __init__(self, canvas, x, y, vitesse = 10, largeur=120, hauteur=20, couleur="blue"):
         self.canvas = canvas
         self.x = x 
         self.y = y
         self.largeur = largeur
         self.hauteur = hauteur
-        self.couleur = couleur
-        self.vitesse = vitesse  # déplacement par touche
+        self.couleur = couleur  
+        self.vitesse = vitesse  #vitesse de déplacement du pad
         self.id = None
         self.moving_left = False
         self.moving_right = False
 
+    #fonction permettant l'affichage du pad 
     def afficher(self): 
         self.id = self.canvas.create_rectangle(
             self.x, self.y, self.x + self.largeur, self.y + self.hauteur, fill=self.couleur
         )
 
+    """ ces 4 fonctions sont appeler par .bind (ligne 104 a 107 de main2)
+    elle fixe des flags (moving_lef et moving_right) qui sont ensuite utiliser par la fonction update pour déplacer la balle.
+    """
     def start_move_left(self, event=None):
         self.moving_left = True
 
@@ -44,14 +50,18 @@ class Pad:
 
     def start_move_right(self, event=None):
         self.moving_right = True
- 
 
     def stop_move_right(self, event=None):
         self.moving_right = False
 
+    # retourne les 2coordonées du pad. 
     def get_coords(self):
         return self.canvas.coords(self.id)
     
+    """
+    regarde l'état des flags et la position du pad: suivant les conditions le pad est déplacer , ou non.
+    cette fonction s'éxecute toutes les 15 ms
+    """
     def update(self):
         if self.moving_left and self.x > 0 :
             self.canvas.move(self.id, -self.vitesse, 0)
@@ -59,4 +69,4 @@ class Pad:
         if self.moving_right and self.x < 1000-self.largeur:
             self.canvas.move(self.id, self.vitesse, 0)
             self.x += self.vitesse
-        self.canvas.after(5, self.update) 
+        self.canvas.after(15, self.update) 
